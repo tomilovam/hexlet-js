60ecc732d91ce9161cccca43a4fbf0e6a6a57029
Мария Томилова <tomilovam@Noutbuk-Maria.local>
2024-08-07 09:31:48 +0300

    Moved logic to src/index.js and updated main entry point

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-import _ from 'lodash';
-console.log(_.last(['one', 'two']));
+import main from './src/index.js';
+
+main();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import _ from "lodash";
+function main() {
+  console.log(_.last(["one", "two"]));
+}
+
+export default main;
